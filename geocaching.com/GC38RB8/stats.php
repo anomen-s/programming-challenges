754f87adf9d8196f2e6a62e2742ab2075fb9b950
anomen <ludek.h@gmail.com>
2012-06-02 00:13:44 +0200

    geocaching.com/fallout - dev - db

--- geocaching.com/GC38RB8/stats.php
+++ geocaching.com/GC38RB8/stats.php
@@ -5,36 +5,85 @@ require_once('config.php');
 $link = mysql_connect ($DB_HOST, $DB_USER, $DB_PASS);
 
 if (!$link) {
-    echo 'Could not connect: ' . mysql_error();
+    echo 'Could not connect to database: ' . mysql_error();
     die();
 }
 
 Header('X-Status: Mysql Connected successfully');
 
-//$res = mysql_query("SHOW DATABASES");
-//while ($row = mysql_fetch_array($res)) { echo "DB: ".$row['Database'] . "<br />\n"; }
+mysql_select_db($DB_DB, $link) or die("Unable to select database: ". mysql_error());
 
-mysql_select_db($DB_DB, $link);
-echo  "<br />ERROR:" . mysql_error();
+if ($_REQUEST['db_create'] == "1234") {
+  echo 'creating db<br />';
 
-//$res = mysql_query("SHOW TABLES");
-//while ($row = mysql_fetch_array($res)) { echo "table: " . $row[0] . "<br />\n"; }
+ $result = mysql_query('DROP TABLE  fallout_stats')
+	or die("Unable to drop table: ". mysql_error());;
 
-if ($_REQUEST['create'] == "1") {
-echo 'creating';
  $result = mysql_query('
-CREATE TABLE IF NOT EXISTS fallout_statistika (
-login varchar(60),
-klice varchar(1000),
-penize  int,
-jidlo int,
-karma int
-)');
-
+	CREATE TABLE IF NOT EXISTS fallout_stats (
+	id      integer not null AUTO_INCREMENT,
+	cas	datetime,
+	invalid	varchar(200),
+	login	varchar(100),
+	klice	varchar(200),
+	penize	integer,
+	jidlo	integer,
+	karma	integer,
+	perky	varchar(200),
+	skore	integer,
+	ip	varchar(20),
+	ua	varchar(300),
+	cert	varchar(200),
+	PRIMARY KEY (id)
+	)')
+	or die("Unable to create table ". mysql_error());
 }
-echo  "ERROR:" . mysql_error();
 
-$res=mysql_query("select count(*) from statistiky");
-echo "rows:" . mysql_num_rows($res);
+$res=mysql_query("select count(*) from fallout_stats");
 
-mysql_close($link);
+function dbstats_access(&$U)
+{
+    $params = array('login','klice','penize','jidlo','karma');
+    foreach ($params as $p) {
+	$U["${p}_mysql"] = mysql_real_escape_string($U["${p}"]);
+    }
+    $sql = 'INSERT INTO fallout_stats (cas, ip, ua, invalid ';
+    foreach ($params as $p) {
+	$sql .= ", $p";
+    }
+    $sql .= ") \nVALUES (now(), '${_SERVER['REMOTE_ADDR']}', '${_SERVER['HTTP_USER_AGENT']}', ${U['invalid']} ";
+    foreach ($params as $p) {
+	$sql .= ", '";
+	$sql .= $U["${p}_mysql"] ;
+	$sql .= "'";
+    }
+    $sql .= ")";
+//    echo $sql;
+    
+    $result = mysql_query($sql)
+	or die("Unable to execute insert: ". mysql_error());
+    
+    $id = mysql_insert_id();
+    $U['id'] = $id;
+    return $id;
+}
+
+function dbstats_update(&$U, $cert)
+{
+    $id = $U['id'];
+    $invalid = empty($U['invalid']) ? 'null' : "'${U['invalid']}'";
+    $perky = implode(',', $U['perky']);
+    $sql = " UPDATE fallout_stats ";
+    $sql .= " SET ";
+    $sql .= "  skore = ${U['skore']}, ";
+    $sql .= "  cert = '$cert', ";
+    $sql .= "  invalid = $invalid, ";
+    $sql .= "  perky = '$perky'" ;
+    
+    $sql .= " WHERE (skore IS NULL) AND (id = $id) ";
+    
+    $result = mysql_query($sql)
+	or die("Unable to execute update: ". mysql_error());
+    
+}
+
