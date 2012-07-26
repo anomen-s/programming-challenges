94d054c9ad398818887039846dbc5dad5e43a7ab
anomen <ludek.h@gmail.com>
2012-07-26 22:47:55 +0200

    gc-fallout - certificate

--- geocaching.com/GC38RB8/cert.php
+++ geocaching.com/GC38RB8/cert.php
@@ -13,7 +13,9 @@ $token = $_SERVER["QUERY_STRING"];
 
 $U = decodeToken($token);
 
-$certimg = imagecreatetruecolor(PERK_WIDTH*PERK_COLS+100, PERK_HEIGHT*PERK_ROWS+100);
+$C_WIDTH = PERK_WIDTH*PERK_COLS + (PERK_COLS-1)*PERK_PADDING + 2*CERT_BORDER;
+$C_HEIGHT = PERK_HEIGHT*PERK_ROWS + (PERK_ROWS-1)*PERK_PADDING + 2*CERT_BORDER;
+$certimg = imagecreatetruecolor($C_WIDTH, $C_HEIGHT);
 $bg = imagecolorallocate($certimg, 0xd0, 0xd0, 0xd0);
 $red_color = imagecolorallocate($certimg, 0xff, 0x00, 0x00);
 $text_color = imagecolorallocate($certimg, 0x00, 0x7f, 0x00);
@@ -21,13 +23,16 @@ $ii = chr(0xED);
 
 imagefill($certimg, 0, 0, $bg);
 
+ $bg_img = imagecreatefrompng("img/cert_bg.png");
+// imagecopy($certimg, $bg_img, 0, 0, 0, 0,  $C_WIDTH, $C_HEIGHT);
+
 
 $i = 0;
 foreach($U['perky'] as $perk) {
  $p = imagecreatefromjpeg("perky/$perk.jpg");
- $x = ($i % PERK_COLS) * PERK_WIDTH;
- $y = floor($i / PERK_COLS) * PERK_HEIGHT;
- imagecopy($certimg, $p, $x + 50 , $y + 50, 0, 0,  PERK_WIDTH, PERK_HEIGHT);
+ $x = ($i % PERK_COLS) * (PERK_WIDTH + PERK_PADDING);
+ $y = floor($i / PERK_COLS) * (PERK_HEIGHT + PERK_PADDING);
+ imagecopy($certimg, $p, $x + CERT_BORDER , $y + CERT_BORDER, 0, 0, PERK_WIDTH, PERK_HEIGHT);
  imagedestroy($p);
  $i++;
 }
