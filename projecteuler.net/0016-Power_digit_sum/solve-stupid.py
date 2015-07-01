dd0d3a72e357023def094fd37ad799e10e64769b
anomen <ludek.h@gmail.com>
2015-07-02 01:16:09 +0200

    projecteuler.net - 16 - updates

---
+++ projecteuler.net/0016-Power_digit_sum/solve-stupid.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2**1000?
+
+num = map(int, str(2**1000))
+
+print(num)
+print(sum(num))
