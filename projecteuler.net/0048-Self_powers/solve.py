011d005401e8ccdd8bee0055f617b56819e03a35
anomen <ludek.h@gmail.com>
2015-08-26 01:02:43 +0200

    comments

--- projecteuler.net/0048-Self_powers/solve.py
+++ projecteuler.net/0048-Self_powers/solve.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+'''
+The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+'''
+
 N=1000
 DIGITS=10
 
