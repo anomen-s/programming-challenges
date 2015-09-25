01dc8d48a588c0f6031ceed887232cfb9bf13dc6
anomen <ludek.h@gmail.com>
2015-09-25 15:37:15 +0200

    cleanup

--- projecteuler.net/0030-Digit_fifth_powers/solve.py
+++ projecteuler.net/0030-Digit_fifth_powers/solve.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+'''
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+ 1634 = 14 + 64 + 34 + 44
+ 8208 = 84 + 24 + 04 + 84
+ 9474 = 94 + 44 + 74 + 44
 
+As 1 = 14 is not a sum it is not included.
 
-# cannot have 7 digits, because 7*(9**5) has only 6 digits
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+Observation:
+- cannot have 7 (or more) digits, because 7*(9**5) has only 6 digits
+'''
 TOP=6*(9**5)
 
 DEBUG = True
