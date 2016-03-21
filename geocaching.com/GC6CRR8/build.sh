bc84698ebdc30d757d644da9c9e91294afe728b4
anomen <ludek.h@gmail.com>
2016-03-21 09:30:32 +0100

    geocaching.com - infinity

--- geocaching.com/GC6CRR8/build.sh
+++ geocaching.com/GC6CRR8/build.sh
@@ -23,6 +23,12 @@ echo "JavaScript.."
 java -jar target/yuicompressor.jar infinity.js > target/io.js
 
 head -n 5 infinity.html > target/i.html
+echo '<!--' >> target/i.html
+git rev-parse HEAD >> target/i.html
+echo " // "  >> target/i.html
+date >> target/i.html
+
+echo '-->' >> target/i.html
 
 echo '<style type="text/css">' >> target/i.html
 cat target/io.css >> target/i.html
@@ -36,5 +42,5 @@ echo '</script>' >> target/i.html
 
 tail -n +10 infinity.html >> target/i.html
 
-xmllint --noblanks --xmlout --dropdtd  target/i.html -o target/ix.html
+#xmllint --noblanks --xmlout --dropdtd  target/i.html -o target/ix.html
 tr -d '\n' < target/i.html > target/infinity.html
