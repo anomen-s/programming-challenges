e1f4d27b3af18150a7288b41fa251042251425fb
anomen <ludek.h@gmail.com>
2020-08-05 10:40:23 +0200

    projecteuler.net cleanup

--- projecteuler.net/0006-Sum_square_difference/formula.py
+++ projecteuler.net/0006-Sum_square_difference/formula.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
 import math;
 
 n=100
