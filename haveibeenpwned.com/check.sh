1c932d2713348b15f75eb4e3da49fd0580dffe67
anomen <ludek.h@gmail.com>
2021-04-12 17:25:20 +0200

    haveibeenpwned.com

---
+++ haveibeenpwned.com/check.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo Enter password:
+read -s P
+
+HASH=`echo -n "$P" | sha1sum`
+
+echo $HASH
+
+PREFIX=`expr substr "$HASH" 1 5`
+REST=`expr substr "$HASH" 6 35`
+
+echo checking $PREFIX and $REST:
+echo ""
+wget https://api.pwnedpasswords.com/range/$PREFIX -q -O - | grep -i $REST
+
